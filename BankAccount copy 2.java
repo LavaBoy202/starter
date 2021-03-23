e62a1f085f50755ab500f2a38da79f24bcc32236
LavanNithi <lnithian@uwaterloo.ca>
2021-03-23 09:14:47 -0400

    updates

--- BankAccount copy 2.java
+++ BankAccount copy 2.java
@@ -10,8 +10,8 @@ public class BankAccount {
         {
             //If the deposit is under 100 then the account will be charged with an additional $2.00
             System.out.println("Your account balance falls under the required ammount which cost a fee of $2.00");
-            finalammount = ammount1 - 2.00;
-            //Deduction calculation takes place  
+            finalammount = ammount1 - 2.00;  
+            //Deduction calculation takes place
 
         }
         else
